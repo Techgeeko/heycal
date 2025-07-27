6729bdd818ef0d5aab13209ee21d3353e21d3b4a
Techgeeko <agulonyemicheal9@gmail.com>
2025-07-27 07:16:53 +0100

    Wrapped chatComponent with CalendarProvider

--- app/(routes)/chat/page.tsx
+++ app/(routes)/chat/page.tsx
@@ -3,13 +3,16 @@
 import ChatComponent from './_component/page';
 import { Loader2 } from "lucide-react";
 import { Suspense } from "react";
+import { CalendarProvider } from "@/components/calendar-provider";
 
 export default function Chat() {
   return (
     <div>
       <Suspense fallback={<Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />}>
         <div>
-          <ChatComponent />
+          <CalendarProvider>
+            <ChatComponent />
+          </CalendarProvider>
         </div>
       </Suspense>
     </div>
